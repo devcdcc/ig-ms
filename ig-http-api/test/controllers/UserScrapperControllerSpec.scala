99ccb5ed1164c8fd020a7f1281933eb67ebcca13
Carlos Daniel Cañon Carrero <daniel.canon@redb.ee>
2019-07-14 02:19:15 -0300

    add coverage to others UserScrapperController

--- ig-http-api/test/controllers/UserScrapperControllerSpec.scala
+++ ig-http-api/test/controllers/UserScrapperControllerSpec.scala
@@ -30,36 +30,45 @@ class UserScrapperControllerSpec extends PlaySpec with MockitoSugar {
       cc = Helpers.stubControllerComponents(),
       publisher = publisher
     )
-  "For scrapUser" should {
-    "return unauthorized response when is not authenticated" in {
-      //given
-      val userId   = "123123"
-      val url      = s"/user/$userId"
-      val expected = UNAUTHORIZED
 
-      //when
-      val result = subject.scrapUser(userId).apply(FakeRequest(POST, url))
+  val userId = "123123"
 
-      //then
-      status(result) mustBe expected
+  def testPostAction(_userId: String, suffix: String = "")(action: String => Action[AnyContent]) =
+    s"For scrapping user data :$suffix" should {
+      "return unauthorized response when is not authenticated" in {
+        //given
+        val userId   = _userId
+        val url      = s"/user/$userId$suffix"
+        val expected = UNAUTHORIZED
+
+        //when
+        val result = action(userId).apply(FakeRequest(POST, url))
+
+        //then
+        status(result) mustBe expected
+      }
+      "return json when everything is okay" in {
+        //given
+        val userId = _userId
+        val url    = s"/user/$userId$suffix"
+        implicit val simpleStringMessageValueConverter: MessageValueConverter[Json, String] =
+          new CirceToStringMessageValueConverter
+        val user                                   = UserRequest(userId = userId, id = Option(random.generate()))
+        val message: Message[String, Json, String] = Message(subject.userScrapperTopic, user.asJson)
+        val expected                               = user.asJson.noSpaces
+
+        //when
+        user.id.foreach(id => when(random.generate()) thenReturn id)
+        when(publisher.sendAsync(message)) thenReturn Future.successful(message)
+
+        //then
+        val result = action(userId).apply(FakeRequest(POST, url).addAuthentication(userId))
+        contentAsString(result) mustBe expected
+        status(result) mustBe ACCEPTED
+      }
     }
-    "return json when everything is okay" in {
-      //given
-      val userId = "123123"
-      val url    = s"/user/$userId"
-      implicit val simpleStringMessageValueConverter: MessageValueConverter[Json, String] =
-        new CirceToStringMessageValueConverter
-      val user                                   = UserRequest(userId = userId, id = Option(random.generate()))
-      val message: Message[String, Json, String] = Message(subject.userScrapperTopic, user.asJson)
-      val expected                               = user.asJson.noSpaces
-
-      //when
-      user.id.foreach(id => when(random.generate()) thenReturn id)
-      when(publisher.sendAsync(message)) thenReturn Future.successful(message)
-
-      //then
-      val result = subject.scrapUser(userId).apply(FakeRequest(POST, url).addAuthentication(userId))
-      contentAsString(result) mustBe expected
-    }
-  }
+  testPostAction(userId)(subject.scrapUser)
+  testPostAction(userId, "/media")(subject.scrapMedia)
+  testPostAction(userId, "/following")(subject.scrapFollowing)
+  testPostAction(userId, "/followers")(subject.scrapFollowers)
 }
